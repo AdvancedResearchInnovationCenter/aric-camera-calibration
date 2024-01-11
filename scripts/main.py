30dffd63af16d4ff3fbc666bd60cf83a85205e13
HussainMSajwani <hussainmsajwani@gmail.com>
2024-01-11 18:23:28 +0400

    data collection using single aruco marker

--- scripts/main.py
+++ scripts/main.py
@@ -11,12 +11,17 @@ if __name__ == '__main__':
     calibration_config_file = rospy.get_param('/camera_calibration/calibration_config')
     data_collector = CameraCalibrationDataCollection(calibration_config_file)
     if not data_collector.use_existing_data:
+        print(1)
         _thread.start_new_thread(data_collector.robot.run_node, ())
+        print(2)
         _thread.start_new_thread(data_collector.collect_data, ())
 
     while not rospy.is_shutdown():
         if data_collector.data_collected:
+            if data_collector.calibration_config['calibration_target']['type'] == 'aruco':
+                raise NotImplementedError('Aruco calibration not implemented yet. Only collecting data is implemented.')
             # robot.cleanup()
+            # print(data_collector.calibration_config)
             calibrator = CameraCalibrator(data_collector.calibration_config)
             calibrator.calibrate_camera_intrinsics()
             calibrator.calibrate_camera_extrinsics()
